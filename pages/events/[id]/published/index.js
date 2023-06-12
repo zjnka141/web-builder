7c68d2dc7eb7e1f6e11a6e84d57c377c3a3400a2
vuidoan <zjnka141@gmail.com>
2023-06-12 10:07:15 +0700

    Add column blocks and swiper plugin

--- pages/events/[id]/published/index.js
+++ pages/events/[id]/published/index.js
@@ -4,6 +4,7 @@ import supabase from "../../../../supabase";
 
 import { ContentProvider } from '../../../../lib/destack/build/browser'
 import Script from "next/script";
+import Head from "next/head";
 
 export const getStaticPaths = async () => {
   const {data, error} = await supabase.from('Event').select('id').order('updated_at', { ascending: false }).limit(100)
@@ -21,11 +22,50 @@ export const getStaticProps = async ({params}) => {
 
 
 export default function Page(props) {
-  
   return (
     <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/swiper@7/swiper-bundle.min.css"
+        />
+      </Head>
       <ContentProvider {...props} />
       <Script src="https://kit.fontawesome.com/777c2040eb.js" />
+      <Script src="//unpkg.com/alpinejs"></Script>
+      <Script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></Script>
+      <Script>
+        {`
+          const initLib = function () {
+            const swiper = new Swiper(".mySwiper", {
+              slidesPerView: 1,
+              spaceBetween: 30,
+              centeredSlides: true,
+              autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+              },
+              pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+              },
+              navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+              },
+            });
+            console.log("swiper :>> ", swiper);
+          };
+          if (typeof Swiper == "undefined") {
+              const script = document.createElement("script");
+              script.onload = initLib;
+              script.src = "https://unpkg.com/swiper@7/swiper-bundle.min.js";
+              document.body.appendChild(script);
+          } else {
+              initLib();
+          }
+        `}
+      </Script>
     </>
   )
 }
